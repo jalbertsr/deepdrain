c8311f1ddf46d5fb27467904fa80415e21aa2ea1
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-02-10 12:18:17 +0100

    changed export

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict'
 
-module.exports = {
-  deep: function (obj, path, _default) {
-    return path
-      .split(/[. | [\]]/)
-      .filter(path => path)
-      .reduce(function (acc, path) {
-        if (acc) return acc[path] || _default
-        else return acc || _default
-      }, obj)
-  }
+module.exports = function (obj, path, _default) {
+  return path
+    .split(/[. | [\]]/)
+    .filter(path => path)
+    .reduce(function (acc, path) {
+      if (acc) return acc[path] || _default
+      else return acc || _default
+    }, obj)
 }
