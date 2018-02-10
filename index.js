3b149fc61f4fc8809a6904084af56bd38cdfee04
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-02-10 01:57:20 +0100

    main function

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function deep (obj, path, _default) {
+  return path
+    .split(/[. | [\]]/)
+    .filter(path => path)
+    .reduce(function (acc, path) {
+      if (acc) return acc[path] || _default
+      else return acc || _default
+    }, obj)
+}
