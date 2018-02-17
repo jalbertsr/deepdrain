b90cfb79e14fa35740e69b27ea1384797fd1c2bb
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-02-17 23:29:53 +0100

    changed test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-// @flow
-
 'use strict'
 
-function deep(obj: Object, path: string, _default: any): any {
+function deep(obj, path, _default) {
   return path
-    .split(/[. | [\]]/)
-    .filter(path => path)
-    .reduce(function(acc: Object, path: string) {
+    .reduce(function(acc, path) {
       if (acc) return acc[path] || _default
       else return acc || _default
     }, obj)
---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect
+var deep = require('./index')
+
+describe('deepdrain', function () {
+  it('should return the value from the given object path', 
+    function () {
+      expect(true).to.be.true
+    })
+})
