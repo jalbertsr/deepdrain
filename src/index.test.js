30b1573ca21ca73fa49bc9ed37ea82d81d9f8552
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-02-18 01:20:08 +0100

    update travis ndoe version

--- src/index.test.js
+++ src/index.test.js
@@ -1,32 +1,32 @@
 'use strict'
 
-const expect = require('chai').expect
-const deep = require('./index')
+var expect = require('chai').expect
+var deep = require('./index')
 
 describe('should return the value from the given path', function () {
   it('simple object path: "animal.cat"', function () {
-    const obj = {
+    var obj = {
       animal: {
         cat: 'meow',
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'dog'])
+    var result = deep(obj, ['animal', 'dog'])
     expect(result).to.equal('woof')
   })
   it('simple array path: "animal.cat[2]"', function () {
-    const obj = {
+    var obj = {
       animal: {
         cat: ['meow', 'miau', 'nya-ong', 'meu'],
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'cat', '2'])
+    var result = deep(obj, ['animal', 'cat', '2'])
     expect(result).to.equal('nya-ong')
   })
 
   it('complex path: "animal.cats[2].name"', function () {
-    const obj = {
+    var obj = {
       animal: {
         cats: [
           {
@@ -49,12 +49,12 @@ describe('should return the value from the given path', function () {
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'cats', '2', 'name'])
+    var result = deep(obj, ['animal', 'cats', '2', 'name'])
     expect(result).to.equal('Muggy')
   })
 
   it('matrix path: "animal.cats[2][1]"', function () {
-    const obj = {
+    var obj = {
       animal: {
         cats: [
           ['monet', 'meow'],
@@ -65,12 +65,12 @@ describe('should return the value from the given path', function () {
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'cats', '3', '1'])
+    var result = deep(obj, ['animal', 'cats', '3', '1'])
     expect(result).to.equal('meu')
   })
 
   it('default value in a nonexistent property: "animal.cat.name"', function () {
-    const obj = {
+    var obj = {
       animal: {
         cat: {
           sound: 'miau'
@@ -78,28 +78,28 @@ describe('should return the value from the given path', function () {
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'cat', 'name'], 'Ben')
+    var result = deep(obj, ['animal', 'cat', 'name'], 'Ben')
     expect(result).to.equal('Ben')
   })
 
   it('default value in a null object: "animal.cat.name"', function () {
-    const obj = {
+    var obj = {
       animal: {
         cat: null,
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'cat', 'name'], 'Ben')
+    var result = deep(obj, ['animal', 'cat', 'name'], 'Ben')
     expect(result).to.equal('Ben')
   })
 
   it('default value in an undefined object: "animal.cat.name"', function () {
-    const obj = {
+    var obj = {
       animal: {
         dog: 'woof'
       }
     }
-    const result = deep(obj, ['animal', 'cat', 'name'], 'Ben')
+    var result = deep(obj, ['animal', 'cat', 'name'], 'Ben')
     expect(result).to.equal('Ben')
   })
 })
